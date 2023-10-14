fe7933644adc3341d11fcdb90969ac282fd7e2fc
Suvasish Jana <suvasishjana06@gmail.com>
2023-10-14 13:20:47 +0530

    University Management System

---
+++ src/universitymanagementsystemnew/Conn.java
@@ -0,0 +1,20 @@
+package universitymanagementsystemnew;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "suvasish@2002");
+            s = c.createStatement();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
