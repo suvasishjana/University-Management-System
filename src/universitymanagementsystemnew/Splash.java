2d20b5f664aa7444e0df5287f40e189cd0daab16
Suvasish Jana <suvasishjana06@gmail.com>
2023-11-17 11:14:37 +0530

    University Management System

--- src/universitymanagementsystemnew/Splash.java
+++ src/universitymanagementsystemnew/Splash.java
@@ -7,7 +7,7 @@ public class Splash extends JFrame implements Runnable {
     Thread t;
     Splash () {
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/first.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/svu.jpeg"));
         Image i2 = i1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
